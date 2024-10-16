b45123cb9476f2083834f25dfa39720e1a1cd3a1
gedw99 <gedw99@gmail.com>
2024-10-17 10:15:06 +1100

    initial structure

--- Makefile
+++ Makefile
@@ -13,17 +13,15 @@ include spok.mk
 this: this-dep this-src this-bin 
 
 this-dep:
-	mkdir -p $(BIN_ROOT)
+	
 	@echo $(BIN_ROOT_NAME) >> .gitignore
 
 	#brew install goreleaser/tap/goreleaser
 
 this-src: spok-src
 
-
-this-bin: spok-bin
+this-bin-del:
+	rm -rf $(BIN_ROOT)
+	mkdir -p $(BIN_ROOT)
+this-bin: this-bin-del spok-bin
 	
-install:
-	cp $(BIN_ROOT)/spok $(GOPATH)/bin/spok
-install-del:
-	rm -rf $(GOPATH)/bin/spok
--- spok.mk
+++ spok.mk
@@ -5,18 +5,20 @@ spok-print:
 
 spok-all: spok-src spok-bin 
 
-
 spok-src:
 	rm -rf spok
 	git clone https://github.com/FollowTheProcess/spok
 	@echo spok >> .gitignore
 
 spok-bin:
-	rm -rf $(BIN_ROOT)
 	touch go.work
 	go work use spok
-	mkdir -p $(BIN_ROOT)
 	cd spok && goreleaser build --single-target --skip=before --snapshot --clean --output $(BIN_ROOT)/spok
+
+spok-install:
+	cp $(BIN_ROOT)/spok $(GOPATH)/bin/spok
+spok-install-del:
+	rm -rf $(GOPATH)/bin/spok
 
 spok-run:
 	spok
